0035c51b9c529a36a1a3c64128b9e378eeacb503
alaghusundhar <alaghusundhar@hotmail.com>
2020-03-29 23:11:37 -0500

    Adding Reverse String

---
+++ reverse_string.py
@@ -0,0 +1,14 @@
+def reverse(text):
+    a = ""
+    print(len(text))
+    for i in range(1,len(text)+1):
+        a += text[len(text) - i]
+        print(a)
+    return a
+
+def reverseword(text):
+    str=""
+    for i in len(text):
+        a +=text[len(text)]
+
+reverse("Hello")
